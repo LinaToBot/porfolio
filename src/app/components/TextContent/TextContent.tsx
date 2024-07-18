56d6e161c332506bed51b824fad46b2695c802d8
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-18 01:17:18 -0600

    TextContent created

---
+++ src/app/components/TextContent/TextContent.tsx
@@ -0,0 +1,29 @@
+// Module style.
+import styles from "./TextContent.module.css";
+// Utils.
+import { space } from "../../utils/utils";
+
+interface TextContentProps {
+  heading1?: string;
+  heading2?: string;
+  paragraph: string;
+}
+
+export const TextContent: React.FC<TextContentProps> = ({
+  heading1,
+  heading2,
+  paragraph,
+}) => {
+  return (
+    <div className={styles.content}>
+      <h1
+        className={`${space.className} ${
+          heading1 ? styles.heading1 : styles.heading2
+        } mb-[2rem]`}
+      >
+        {heading1 || heading2}
+      </h1>
+      <p className={`${styles.text}`}>{paragraph}</p>
+    </div>
+  );
+};
