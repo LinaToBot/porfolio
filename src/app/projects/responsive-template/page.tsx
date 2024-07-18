07b2f273ebdb79654e1b698e515f87d1dea119d2
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-18 16:11:58 -0600

    responsive styles

--- src/app/projects/responsive-template/page.tsx
+++ src/app/projects/responsive-template/page.tsx
@@ -1,14 +1,15 @@
 // Libraries.
 import Image from "next/image";
-// Fonts.
-import { Space_Grotesk } from "next/font/google";
 // Module style.
 import styles from "./page.module.css";
 // Component.
 import { TextContent } from "@/app/components/TextContent/TextContent";
-// Public.
-import design from "../../../../public/designs.svg";
 import { Footer } from "@/app/components/Footer/Footer";
+// Public.
+import design from "../../../../public/responsive_design.png";
+import widthImage from "../../../../public/flexible-width.png";
+import limitsImage from "../../../../public/limits.png";
+import breakpointsImage from "../../../../public/breakpoints.png";
 
 const ResponsivePage: React.FC = () => {
   return (
@@ -20,8 +21,13 @@ const ResponsivePage: React.FC = () => {
             paragraph="This is a mini project where you can learn how build a responsive page."
           />
         </div>
-        <div className={styles.responsiveDiv}>
-          <Image src={design} alt="responsive design" />
+        <div className={styles.responsiveImage}>
+          <Image
+            src={design}
+            alt="responsive design"
+            objectFit="scale-down"
+            width={600}
+          />
         </div>
       </div>
 
@@ -32,14 +38,13 @@ const ResponsivePage: React.FC = () => {
             paragraph="First, you have to set the width of the elements with a flexible measure, for example: 100%."
           />
         </div>
-        <div className={styles.responsiveDiv}>
-          <p>This div have a width to 100%</p>
-
-          <p>
-            This p have a maximum width with 30rem.
-            <br />
-            If you want to try, try zooming in and out.
-          </p>
+        <div className={styles.responsiveImage}>
+          <Image
+            src={widthImage}
+            alt="css widths"
+            objectFit="scale-down"
+            width={600}
+          />
         </div>
       </div>
 
@@ -50,7 +55,14 @@ const ResponsivePage: React.FC = () => {
             paragraph="Second, if it`s necessary, set another measures, but now related to the disposition that one element will take respect to the others."
           />
         </div>
-        <div className={styles.responsiveDiv}>Text </div>
+        <div className={styles.responsiveImage}>
+          <Image
+            src={limitsImage}
+            alt="responsive design"
+            objectFit="scale-down"
+            width={600}
+          />
+        </div>
       </div>
 
       <div className={styles.responsivePage}>
@@ -60,7 +72,14 @@ const ResponsivePage: React.FC = () => {
             paragraph="Whether you have decided to implement step two or not, you must use media queries if you want your page to adapt to different devices."
           />
         </div>
-        <div className={styles.responsiveDiv}>Text </div>
+        <div className={styles.responsiveImage}>
+          <Image
+            src={breakpointsImage}
+            alt="responsive design"
+            objectFit="scale-down"
+            width={600}
+          />
+        </div>
       </div>
       <Footer />
     </>
