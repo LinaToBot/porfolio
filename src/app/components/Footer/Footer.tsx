2643833a7add32369d3ed5ff11db30461c7bd825
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-18 16:06:42 -0600

    clean code

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="h-[8vh] flex flex-column justify-center items-center text-[0.9rem] bg-[#0e0e0e] border-t border-[#343434] ">
       <p>
-        &copy; 2024 Lina2Bot. Todos los derechos reservados. Icons by
+        &copy; 2024 Lina2Bot. Todos los derechos reservados. Icons by{" "}
         <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">
           icons8.com
         </a>
