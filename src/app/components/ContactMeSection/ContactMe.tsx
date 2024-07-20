c86b679bbad9713aa1793d3129490341fc60ee43
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-19 20:22:53 -0600

    ContactMe Refactor

--- src/app/components/ContactMeSection/ContactMe.tsx
+++ src/app/components/ContactMeSection/ContactMe.tsx
@@ -21,7 +21,7 @@ export const ContactMe: React.FC = () => {
             blinkingHeading="Contact me"
             paragraphs={
               <>
-                <p className="mb-[1rem] text-[1.5rem]">
+                <p className="mb-[1rem]">
                   If you want to talk to me about development or front-end, I
                   invite you to visit my{" "}
                   <a
@@ -34,7 +34,7 @@ export const ContactMe: React.FC = () => {
                   </a>
                   .
                 </p>
-                <p className="mb-[1rem] text-[1.5rem]">
+                <p className="mb-[1rem]">
                   Additionally, we can read each other on{" "}
                   <a
                     className="highlight-text"
@@ -45,7 +45,7 @@ export const ContactMe: React.FC = () => {
                     Medium.
                   </a>
                 </p>
-                <p className="mb-[1rem] text-[1.5rem]">
+                <p className="mb-[1rem]">
                   Or you can head over to my{" "}
                   <a
                     className="highlight-text"
@@ -72,59 +72,6 @@ export const ContactMe: React.FC = () => {
           />
         }
       />
-      {/* <div className="w-full text-justify md:text-left mb-0 w-2/2">
-        <h2
-          className={`${space.className} blinking titles text-[3.5rem] font-[600]`}
-        >
-          Contact me
-        </h2>
-        <p className="mb-[1rem] text-[1.5rem]">
-          If you want to talk to me about development or front-end, I invite you
-          to visit my{" "}
-          <a
-            className="highlight-text"
-            href={linkedinUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a>
-          .
-        </p>
-        <p className="mb-[1rem] text-[1.5rem]">
-          Additionally, we can read each other on{" "}
-          <a
-            className="highlight-text"
-            href={mediumURL}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Medium.
-          </a>
-        </p>
-        <p className="mb-[1rem] text-[1.5rem]">
-          Or you can head over to my{" "}
-          <a
-            className="highlight-text"
-            href={gitHubURL}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>{" "}
-          to start a joint project.
-        </p>
-      </div>
-      <div className=" w-full p-4 md: w-1/2 flex justify-center p-4 ">
-        <Image
-          src={faceImage}
-          alt="face icon"
-          className="dark:invert cursor-pointer"
-          width={300}
-          height={300}
-          priority
-        />
-      </div> */}
     </section>
   );
 };
