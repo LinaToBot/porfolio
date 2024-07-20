cc4f67bc0b1d57abaa819db6b99fcb9eba3538ce
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-19 19:49:44 -0600

    Footer refactor

--- src/app/components/shared/Footer/Footer.tsx
+++ src/app/components/shared/Footer/Footer.tsx
@@ -1,7 +1,10 @@
+// Styles.
+import styles from "./Footer.module.css";
+
 export const Footer: React.FC = () => {
   return (
-    <footer className="h-[8vh] flex flex-column justify-center items-center text-[0.9rem] bg-[#0e0e0e] border-t border-[#343434] ">
-      <p>
+    <footer className={styles.footerSection}>
+      <p className="text-center">
         &copy; 2024 Lina2Bot. Todos los derechos reservados. Icons by{" "}
         <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">
           icons8.com
