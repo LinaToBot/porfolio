291037e71630c9244f23a40e665d3a3782e6f254
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-07-22 16:11:12 -0600

    height updated

--- src/app/coming-soon-page/page.tsx
+++ src/app/coming-soon-page/page.tsx
@@ -3,7 +3,7 @@ import styles from "./comingSoonPage.module.css";
 
 const ComingSoonPage: React.FC = () => {
   return (
-    <main className=" flex flex-column justify-center items-center h-[78vh]">
+    <main className=" flex flex-column justify-center items-center h-[85vh]">
       <div className={styles.container}>
         <h1 className="color-[#fff] text-center mt-[4rem]">Coming soon!</h1>
       </div>
